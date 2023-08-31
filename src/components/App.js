9a475e65b1ff1a3266f06205ca6d3a41994cff54
leon <leon.waga@student.moringaschool.comgit config --global user.name leongit config --global user.name leon>
2023-09-01 00:51:08 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions,setQuestions] = useState([])
+
+   const url= "http://localhost:4000/questions"
+
+   useEffect(() =>{
+      fetch(url)
+      .then(resp => resp.json())  
+      .then(data =>{
+         console.log(data)
+        return setQuestions(data)
+
+    })
+
+  },[])
 
   return (
     <main>
